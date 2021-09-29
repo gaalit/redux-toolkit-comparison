9e32a4f72c463e29e01f34ed064d3fed52c6e25f
Galit Gerasimov <galit.gerasimov3@gmail.com>
2021-09-29 11:04:58 -0400

    added redux-toolkit

---
+++ src/redux-toolkit.ts
@@ -0,0 +1,88 @@
+import {
+  createSlice,
+  PayloadAction,
+  PayloadActionCreator,
+} from "@reduxjs/toolkit";
+import { Todo } from "./type";
+import { v1 as uuid } from "uuid";
+import { stringify } from "querystring";
+
+// CREATE SLICE - combines our action constants, creates action Types, Action Creators.
+//  all we must do is create Reducers
+
+// > what are the slices of the state (todos, selectedTodo, counter)
+
+const todosIntialState: Todo[] = [
+  {
+    id: uuid(),
+    desc: "Learn React",
+    isComplete: true,
+  },
+  {
+    id: uuid(),
+    desc: "Learn Redux",
+    isComplete: true,
+  },
+  {
+    id: uuid(),
+    desc: "Learn Redux-ToolKit",
+    isComplete: false,
+  },
+];
+
+const todosSlice = createSlice({
+  name: "todos",
+  initialState: todosIntialState,
+  reducers: {
+    // key = name of action Type
+    // value = function that gets returned with the action type
+    create: {
+      reducer: (
+        state,
+        {
+          payload,
+        }: PayloadAction<{ id: string; desc: string; isComplete: boolean }>
+      ) => {
+        state.push(payload);
+      },
+      prepare: ({ desc }: { desc: string }) => ({
+        payload: {
+          id: uuid(),
+          desc,
+          isComplete: false,
+        },
+      }),
+    },
+    edit: (state, { payload }: PayloadAction<{ id: string; desc: string }>) => {
+      const todoToEdit = state.find((todo) => todo.id === payload.id);
+      if (todoToEdit) {
+        todoToEdit.desc = payload.desc;
+      }
+    },
+    toggle: (
+      state,
+      { payload }: PayloadAction<{ id: string; isComplete: boolean }>
+    ) => {
+      const todoToToggle = state.find((todo) => todo.id === payload.id);
+      if (todoToToggle) {
+        todoToToggle.isComplete = payload.isComplete;
+      }
+    },
+    remove: (state, { payload }: PayloadAction<{ id: string }>) => {
+      const index = state.findIndex((todo) => todo.id === payload.id);
+      if (index !== -1) {
+        state.splice(index, 1);
+      }
+    },
+  },
+});
+
+const selectedTodoSlice = createSlice({
+  name: "selectedTodo",
+  initialState: null as string | null,
+  reducers: {
+    select: (state, { payload }: PayloadAction<{ id: string }>) => {
+      const;
+    },
+  },
+});
